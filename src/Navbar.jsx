1bb4800fe3ad14d741b2a4308b94efb2407f587e
HenryRivera01 <hariverao@udistrital.edu.co>
2025-09-19 16:31:57 -0500

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLinks } from './NavLinks'
+export const Navbar = () => {
+  return (
+    <nav className='navbar'> 
+
+    <h5>CONTEXT API</h5>
+    <NavLinks />
+    </nav>
+  )
+}
